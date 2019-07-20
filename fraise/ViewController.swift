a53ea485c851999205a35d847a1b7770b4f9406c
Matthew Wang <matt@matthewwang.me>
2019-07-19 23:06:32 -0700

    creates very basic app - cycles through array of words, shuffles them when done

--- fraise/ViewController.swift
+++ fraise/ViewController.swift
@@ -9,10 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var phrases = [
+        "death star",
+        "salsa",
+        "coffee mug",
+        "hockey stick",
+        "screw",
+        "trampoline",
+        "options",
+        "airpods",
+        "goggles",
+        "pizza",
+        "fried rice",
+        "cutting board",
+        "water bottles",
+        "pineapple"
+    ]
+    var phraseCount = 0;
+    
+    @IBOutlet weak var currentPhrase: UILabel!
+    @IBOutlet weak var newPhraseButton: UIButton!
+    @IBAction func generateNewPhrase(_ sender: Any) {
+        if phraseCount + 1 < phrases.count{
+            phraseCount += 1
+        }
+        else{
+            newPhrases()
+        }
+        currentPhrase.text = phrases[phraseCount];
+    }
+    func newPhrases(){
+        phrases.shuffle();
+        phraseCount = 0
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newPhrases()
+        currentPhrase.text = phrases[phraseCount];
     }
 
 
